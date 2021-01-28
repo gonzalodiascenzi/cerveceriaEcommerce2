5d2ba75453c13093d1d9892c28ab6fad510d4f18
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2021-01-28 16:30:58 -0300

    Cambios

---
+++ routes/products.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/contacto',  (req, res) => {
+  res.render('newViews/contacto');
+})
+
+router.get('/fundadores',  (req, res) => {
+  res.render('newViews/fundadores');
+})
+
+router.get('/eventos',  (req, res) => {
+  res.render('newViews/eventos');
+})
+
+router.get('/cervezas',  (req, res) => {
+  res.render('newViews/cervezas');
+})
+
+router.get('/local',  (req, res) => {
+  res.render('newViews/local');
+})
